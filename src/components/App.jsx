16f1b2a8344a6b2807efacb95c188f9db8ff255f
KovalenkoSerhiy <118220875+KovalenkoSerhiy@users.noreply.github.com>
2023-11-03 00:11:53 +0200

    323

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,38 @@
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
 
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return this.countTotalFeedback() === 0
+      ? 0
+      : (this.state.good * 100) / this.countTotalFeedback;
+  };
+
+  render() {
+    const options = [];
+    for (const key in this.state) {
+      options.push(key);
+    }
+
+    return (
+      <div>
+        <Section title="Please leave feedback"></Section>
+        <Section title="Statistics">
+        
+        </Section>
+      </div>
+    );
+  }
+}
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  totalFeedback,
+  positiveFeedbackPercentage,
+}) => {
+  return (
+    <>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {totalFeedback}</p>
+      <p>Positive Feedback:{positiveFeedbackPercentage}</p>
+    </>
+  );
+};
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  totalFeedback: PropTypes.number.isRequired,
+  positiveFeedbackPercentage: PropTypes.number,
+};
